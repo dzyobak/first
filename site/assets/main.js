5fb6d5c55655682dde58fff94ec431c7834ff65d
dzyobak <ihordzyobak@gmail.com>
2022-05-21 11:32:28 +0300

    jell

--- site/assets/main.js
+++ site/assets/main.js
@@ -1,13 +1,5 @@
-document.body.onload = addElement;
-var my_div = (newDiv = null);
-var a = document.getElementById("note__input");
+var a = document.getElementsByClassName("note__input").value;
 
-function addElement() {
-  var newDiv = document.createElement("div");
-  newDiv.classList.add("container");
-  newDiv.innerHTML =
-    "Your first note: " + document.getElementById("note__input");
-  my_div = document.getElementById("org_div1");
-  document.body.insertBefore(newDiv, my_div);
+function get() {
+  document.getElementById("div__").innerHTML += "Your first note: " + a;
 }
-document.write("jellO!");
