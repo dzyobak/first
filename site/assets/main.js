01dc853769f9258d0eda86fe457f112194c51d79
dzyobak <ihordzyobak@gmail.com>
2022-05-21 11:08:10 +0300

    add textarea and make button with js code

--- site/assets/main.js
+++ site/assets/main.js
@@ -1,7 +1,13 @@
-function uncheckAll() {
-  $('input[type="checkbox"]:checked').prop("checked", false);
-}
+document.body.onload = addElement;
+var my_div = (newDiv = null);
+var a = document.getElementById("note__input");
 
-function like() {
-  document.getElementById("test").style.color = "red";
+function addElement() {
+  var newDiv = document.createElement("div");
+  newDiv.classList.add("container");
+  newDiv.innerHTML =
+    "Your first note: " + document.getElementById("note__input");
+  my_div = document.getElementById("org_div1");
+  document.body.insertBefore(newDiv, my_div);
 }
+document.write("jellO!");
