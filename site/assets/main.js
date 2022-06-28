ca5608a8778ca3129e88727ba46cc97ecacafb82
dzyobak <ihordzyobak@gmail.com>
2022-06-28 13:44:25 +0300

    aa

--- site/assets/main.js
+++ site/assets/main.js
@@ -2,4 +2,3 @@ var note = document.getElementsByClassName("note__input").value;
 function get(value) {
   document.getElementById("div__").innerHTML += "Your first note: " + note;
 }
-document.getElementById(div__).innerHTML = "privet";
